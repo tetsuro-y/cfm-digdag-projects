5881e60cdbb449b67eaf632ec949dd855b2b2668
saitokenichiro <kenichiro.saito@starttoday.jp>
2017-11-28 19:33:38 +0900

    全体の構成を自動化のために整えた 運用で使用するためのテンプレを作成 readmeを作成

---
+++ execute.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source $(cd $(dirname $0) && pwd)/option_check.sh
+
+# プロジェクトの登録
+echo "digdag push ${PJ_NAME} --project ${PJ_DIR} -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint ${DIGDAG_SERVER}"
+digdag push ${PJ_NAME} --project ${PJ_DIR} -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint ${DIGDAG_SERVER} || exit 1
+
+# bigqueryのアクセスキーを設定します
+cp ~/git/zozo-e62ae29b6c4f_cfm.json .
+digdag secrets --project ${PJ_NAME} --set gcp.credential=@zozo-e62ae29b6c4f_cfm.json --endpoint ${DIGDAG_SERVER}
+rm zozo-e62ae29b6c4f_cfm.json
+
+# ワークフローの実行
+echo "digdag start ${PJ_NAME} ${PJ_NAME} --session now --params-file ${PJ_DIR}/config.yml --endpoint ${DIGDAG_SERVER} -p exec_env=${EXECUTE_ENV} -p exec_server=${DIGDAG_SERVER}"
+digdag start ${PJ_NAME} ${PJ_NAME} --session now --params-file ${PJ_DIR}/config.yml --endpoint ${DIGDAG_SERVER} -p exec_env=${EXECUTE_ENV} -p exec_server=${DIGDAG_SERVER}
