36662c4d60f1d6e1a8b13bf4e3dabf0793f5d18b
saitokenichiro <kenichiro.saito@starttoday.jp>
2017-12-25 11:51:01 +0900

    windowsとmacで実行するdigdagを変更した

--- execute.sh
+++ execute.sh
@@ -2,15 +2,30 @@
 
 source $(cd $(dirname $0) && pwd)/option_check.sh
 
+# SERVER SETTING
+case "$(uname -s)" in
+    Linux*)     machine=linux;;
+    Darwin*)    machine=mac;;
+    CYGWIN*)    machine=cygwin;;
+    MINGW*)     machine=mingw;;
+    *)          machine="other"
+esac
+
+if [ "${machine}" = "cygwin"  ] || [ "${machine}" = "mingw"  ]; then
+    DIGDAG=digdag.bat
+else
+    DIGDAG=digdag
+fi
+
 # プロジェクトの登録
 echo "digdag push ${PJ_NAME} --project ${PJ_DIR} -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint ${DIGDAG_SERVER}"
-digdag push ${PJ_NAME} --project ${PJ_DIR} -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint ${DIGDAG_SERVER} || exit 1
+${DIGDAG} push ${PJ_NAME} --project ${PJ_DIR} -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint ${DIGDAG_SERVER} || exit 1
 
 # bigqueryのアクセスキーを設定します
 cp ~/git/zozo-e62ae29b6c4f_cfm.json .
-digdag secrets --project ${PJ_NAME} --set gcp.credential=@zozo-e62ae29b6c4f_cfm.json --endpoint ${DIGDAG_SERVER}
+${DIGDAG} secrets --project ${PJ_NAME} --set gcp.credential=@zozo-e62ae29b6c4f_cfm.json --endpoint ${DIGDAG_SERVER}
 rm zozo-e62ae29b6c4f_cfm.json
 
 # ワークフローのテスト(dry-run)
 echo "digdag start ${PJ_NAME} ${PJ_NAME} --session now --endpoint ${DIGDAG_SERVER} ${PJ_OPTION}"
-digdag start ${PJ_NAME} ${PJ_NAME} --session now --endpoint ${DIGDAG_SERVER} ${PJ_OPTION}
+${DIGDAG} start ${PJ_NAME} ${PJ_NAME} --session now --endpoint ${DIGDAG_SERVER} ${PJ_OPTION}
