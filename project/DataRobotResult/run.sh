8b83bab309abf2bf13f307c2cd66f248306585a0
saitokenichiro <kenichiro.saito@starttoday.jp>
2018-03-16 14:28:00 +0900

    括弧がコマンドに入るとエラーになるので修正した

--- project/DataRobotResult/run.sh
+++ project/DataRobotResult/run.sh
@@ -13,4 +13,4 @@ digdag push ${PJNAME} --project ${SCRIPT_DIR} --endpoint 10.201.161.10:65432
 # rm zozo-e62ae29b6c4f_cfm.json
 
 # ワークフローの実行
-digdag start ${PJNAME} ${PJNAME} --session now --endpoint 10.201.161.10:65432 -p ex_execute_date="" -p ex_model_name="IMPORT_TEST" -p ex_threshold="0.3111"
+digdag start ${PJNAME} ${PJNAME} --session now --endpoint 10.201.161.10:65432 -p ex_execute_date="20180316" -p ex_model_name="Untitled_Project_ENET_Blender_(58+59+57+54+55+56+52+53)_(62)_64.0_Informative_Features_DataRobotPredict" -p ex_threshold="0.3111"
