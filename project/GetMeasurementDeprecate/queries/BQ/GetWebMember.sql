ad2387a357c9d7cd7ca1d650cb31bbffca1cd7ac
kawana <kawana-tomohisa@starttoday.jp>
2018-06-26 22:12:31 +0900

    インデントを修正

--- project/GetMeasurementDeprecate/queries/BQ/GetWebMember.sql
+++ project/GetMeasurementDeprecate/queries/BQ/GetWebMember.sql
@@ -17,30 +17,30 @@ FROM
                     ,device.operatingSystem as DEVICE_OS
                     ,MAX(STRFTIME_UTC_USEC(visitStartTime * 1000000, "%Y/%m/%d %H:%M:%S")) AS VISITTIME
                 FROM
-                        table_date_range([109049626.ga_sessions_],timestamp(DATE_ADD(CURRENT_DATE(), -2, 'MONTH')),timestamp(current_date()))
+                    table_date_range([109049626.ga_sessions_],timestamp(DATE_ADD(CURRENT_DATE(), -2, 'MONTH')),timestamp(current_date()))
                 WHERE
                     hits.customVariables.customVarname = 'memberID'
-                    AND device.mobileDeviceModel != '(not set)'
-                    AND device.mobileDeviceInfo != '(not set)'
-                    AND device.operatingSystem IN ('iOS', 'Android')
+                AND device.mobileDeviceModel != '(not set)'
+                AND device.mobileDeviceInfo != '(not set)'
+                AND device.operatingSystem IN ('iOS', 'Android')
                 GROUP EACH BY
-                MEMBERID
-                ,DEVICE_INFO
-                ,DEVICE_MOBILEDEVICEMODEL
-                ,DEVICE_OS
+                    MEMBERID
+                    ,DEVICE_INFO
+                    ,DEVICE_MOBILEDEVICEMODEL
+                    ,DEVICE_OS
             ) BASE
     ) BASE
 WHERE
     ROWNUM = 1
-    AND (
-        (
-            DEVICE_OS = 'Android'
-            AND NOT REGEXP_MATCH(DEVICE_INFO,'.*Galaxy.*(S5|S7|S8).*')
-            AND NOT REGEXP_MATCH(DEVICE_INFO,'.*Xperia.*(XZ|X Compact|X Performance).*')
-            AND NOT REGEXP_MATCH(DEVICE_INFO,'.*AQUOS.*(R|ZETA).*')
-        )
-        OR (
-            DEVICE_OS = 'iOS'
-            AND DEVICE_MOBILEDEVICEMODEL != 'iPhone'
-        )
+AND (
+    (
+        DEVICE_OS = 'Android'
+    AND NOT REGEXP_MATCH(DEVICE_INFO,'.*Galaxy.*(S5|S7|S8).*')
+    AND NOT REGEXP_MATCH(DEVICE_INFO,'.*Xperia.*(XZ|X Compact|X Performance).*')
+    AND NOT REGEXP_MATCH(DEVICE_INFO,'.*AQUOS.*(R|ZETA).*')
     )
+    OR (
+        DEVICE_OS = 'iOS'
+    AND DEVICE_MOBILEDEVICEMODEL != 'iPhone'
+    )
+)
